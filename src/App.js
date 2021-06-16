e619208c93d294a81e8d2c1fb669e97c9aec46ee
Harshal12355 <harshalts@gmail.com>
2021-06-16 17:17:55 +0400

    Added transition to the drawer and dark mode, header is completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import SettingsIcon from '@material-ui/icons/Settings';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import AssessmentIcon from '@material-ui/icons/Assessment';
+// import { Avatar } from '@material-ui/core';
 import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
 import { IconButton } from '@material-ui/core';
-import NightsStayIcon from '@material-ui/icons/NightsStay';
-import WbSunnyRoundedIcon from '@material-ui/icons/WbSunnyRounded';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
+import PauseIcon from '@material-ui/icons/Pause';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import RestoreIcon from '@material-ui/icons/Restore';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+import Input from '@material-ui/core/Input';
+import TimerIcon from '@material-ui/icons/Timer';
+import Grid from '@material-ui/core/Grid';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { makeStyles } from '@material-ui/core/styles';
-
+import NightsStayIcon from '@material-ui/icons/NightsStay';
+import WbSunnyRoundedIcon from '@material-ui/icons/WbSunnyRounded';
 
 
 function App() {
 
+  //DRAWER TINGS 
   const [openClose, setOpenClose] = useState(false); //opening and closing the drawer
+  const drawerList = {
+    "Report": <AssessmentIcon />, 
+    "Settings": <SettingsIcon />,
+    "Profile": <AccountCircleIcon />,
+  };
+ 
+  //DARK MODE TINGS
   const [darkMode, setDarkMode] = useState(true); //darkmode state
-
   const theme = React.useMemo(
     () =>
       createMuiTheme({
@@ -46,32 +70,49 @@ function App() {
     root: {
       width: 350,
     },
+    header: {
+      display: "flex", 
+      flexDirection: "row",
+    }
   });
 
   const classes = useStyles();
 
   return (
+    
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Drawer transitionDuration={600}  open={openClose} onClose={() => {setOpenClose(false);}}>
+        <List className= {classes.list}>
+          {Object.entries(drawerList).map(([label, icon]) => (
+            <ListItem button key={label}>
+            <ListItemIcon>{icon}</ListItemIcon>
+            <ListItemText primary={label} />
+          </ListItem>
+          ))}
+        </List>
+      </Drawer>
       <div>
         <Header />
       </div>
     </ThemeProvider>
+    
   );
 
   function Header() {
     return (
       <div>
-        <div  style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-          <AppDrawer />
-          <div style={{ display: "flex", flexDirection: "row", justifyContent: "start", marginLeft: "calc(10% - 40px)" }}>
-            <IconButton style = {{ marginLeft: "-2vw", marginRight: "2vw"}} onClick={() => { setOpenClose(true); }}>
+        <div className={classes.header} style={{justifyContent: "space-between", alignItems: "center" }}>
+          {/* Div below is for the hamburger icon and the title */}
+          <div className={classes.header}  style={{justifyContent: "start", marginLeft: "calc(10% - 40px)" }}>
+            <IconButton style={{ marginLeft: "-2vw", marginRight: "2vw" }} onClick={() => { setOpenClose(true); }}>
               <MenuRoundedIcon />
             </IconButton>
             <h1>Pomodoro Timer</h1>
           </div>
-          <div style = {{ marginRight: "calc(10%)"}}>
-            <IconButton onClick={() => {setDarkMode(!darkMode); }}>
+          {/* This is for the darkmode functionality */}
+          <div style={{ marginRight: "calc(10%)" }}>
+            <IconButton onClick={() => { setDarkMode(!darkMode); }}>
               {darkMode === false ? <WbSunnyRoundedIcon /> : <NightsStayIcon />}
             </IconButton>
           </div>
@@ -82,24 +123,6 @@ function App() {
       </div>
     );
   }
-
-  function AppDrawer() {
-    return (
-      <Drawer
-        open={openClose}
-        onClose={() => {
-          setOpenClose(false);
-        }}
-      >
-        <h1>Gandu</h1>
-      </Drawer>
-    );
-  }
-
-  // function DarkModeOnOff() {
-  //   if ()
-  // }
-
 }
 
 export default App;
